5cb007c1048655fc1eea0c968627925ccc928766
Ethan Miller <ethan.w.miller@gmail.com>
2015-07-24 15:47:18 -0400

    version 0.0.0.3 complete, can detect Rails directory now

--- lib/swag.rb
+++ lib/swag.rb
@@ -5,5 +5,10 @@ class Swag
 
   def self.check
     puts "Checking your directory: #{Dir.pwd}"
+    if File.exist?("bin/rails")
+      puts "Rails App detected. Proceeding."
+    else
+      puts "You must be in a Rails root directory for Swag to work."
+    end
   end
 end
