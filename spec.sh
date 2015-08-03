36bd77c2f99770f5bbf0ce5b4292224f15cb11c2
Ethan Miller <ethan.w.miller@gmail.com>
2015-08-03 12:08:24 -0400

    script

---
+++ spec.sh
@@ -0,0 +1,16 @@
+echo "Enter version number"
+read version
+echo "Gem::Specification.new do |s|\n
+  s.name        = 'swag'
+  s.version     = '$version'
+  s.date        = `date +"%Y-%m-%d"`
+  s.summary     = \"Swag. Under development.\"
+  s.description = \"A gem with which to generate YAML from a Rails app. Currently
+  under development.\"
+  s.authors     = [\"Ethan Miller\"]
+  s.email       = \"ethan.w.miller@gmail.com\"
+  s.files       = [\"lib/swag.rb\"]
+  s.homepage    = 'http://rubygems.org/gems/swag'
+  s.license     = \"\"
+  s.executables << 'swag'
+  end" > swag.gemspec 
