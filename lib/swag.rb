f73af993fe628675d36c1a8a546a8da8c3981c9b
Ethan Miller <ethan.w.miller@gmail.com>
2015-08-04 15:22:30 -0400

    added analyze controllers method

--- lib/swag.rb
+++ lib/swag.rb
@@ -5,6 +5,7 @@ class Swag
     	puts "Swag, world."
 	end
 
+	# verifies rails directory
 	def self.check
 		puts "Checking your directory: #{Dir.pwd}"
     	if File.exist?("bin/rails")
@@ -15,10 +16,11 @@ class Swag
     	end
 	end
 
+	# checks that app/controllers can be read
 	def self.checkControllers
     	puts "Checking controllers."
 		begin
-		Dir.foreach("app/controllers") do |c| 
+		Dir.foreach("app/controllers") do |c|
 				puts "Found #{c}"
 		end
     	rescue => ex
@@ -28,6 +30,20 @@ class Swag
 		end
 	end
 
+	# writes specific controller's routes (helper for self.writePaths)
+	# 'name' is the controller's name, 'doc' is the open YAML File to write to
+	def self.analyzeController(name, doc)
+		puts "Analyzing controller: #{name}"
+		# controller = File.open("app/controllers/#{name}", 'r')
+		File.open("app/controllers/#{name}", 'r') do |c|
+			c.each_line do |line|
+				puts "#{name} contains index" if line.include? "def index"
+			end
+		end
+		puts "Closed file." # closed automatically
+	end
+
+	# lists controller paths by reading app/controllers directory
 	def self.writePaths
     	puts "Writing paths."
 		begin
@@ -35,12 +51,14 @@ class Swag
 		doc << "info: Generated with Swag.\n"
 		doc << "paths:\n"
 		Dir.foreach("app/controllers") do |c|
-		unless (c == "." || c == ".." || c == "concerns" ||	c == "application_")
-			c = c.slice(0..(c.index('_') -1))	
-			doc << "    /#{c}:\n"
-		end	
+			unless (c == "." || c == ".." || c == "concerns" ||
+				c == "application_controller.rb")
+				analyzeController(c, doc)
+				c = c.slice(0..(c.index('_') -1))
+				doc << "    /#{c}:\n"
+			end
 		end
-	    doc.close	
+	  doc.close
 		end
 	end
 end
