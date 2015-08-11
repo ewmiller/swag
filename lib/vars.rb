df0e2dc2c05803364224238ab9a45ac0ce7522ae
Ethan Miller <ethan.w.miller@gmail.com>
2015-08-11 11:48:25 -0400

    renamed the vars file cause it wasn't global

---
+++ lib/vars.rb
@@ -0,0 +1,21 @@
+SWAG_VERSION = '0.3.1'
+
+DEFAULT_CONFIG = {
+  :swag => "#{SWAG_VERSION}",
+  :info => {
+    :version => "0.0"
+    :title => ""
+    :description => "An API."
+    :author => {
+      :name => "Example"
+      :contact => "example@example.com"
+    }
+    :license => ""
+  }
+  :host => "localhost:3000"
+  :basepath => "/"
+  :schemes => {
+    "http"
+  }
+  :paths => {}
+}
