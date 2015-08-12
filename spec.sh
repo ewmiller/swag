bdee35734b46e09a041b83d26f471942e59e587e
Ethan Miller <ethan.w.miller@gmail.com>
2015-08-12 14:13:44 -0400

    syntax

--- spec.sh
+++ spec.sh
@@ -18,18 +18,18 @@ echo "Gem::Specification.new do |s|
 echo "SWAG_VERSION = '$version'
 
 \$DEFAULT_CONFIG = {
-  :swag => \"#{SWAG_VERSION}\"
+  :swag => \"#{SWAG_VERSION}\",
   :info => {
-    :version => \"0.0\"
-    :title => \"My API.\"
-    :description => \"An API.\"
+    :version => '0.0',
+    :title => \"My API.\",
+    :description => \"An API.\",
     :author => {
-      :name => \"Example\"
-      :contact => \"example@example.com\"
-    }
-    :license => \"BSD 3-Clause\"
-  }
-  :host => \"localhost:3000\"
-  :basepath => \"/\"
-  :schemes => [\"http\"]
-}\" > lib/vars.rb
+      :name => \"Example\",
+      :contact => \"example@example.com\",
+    },
+    :license => \"BSD 3-Clause\",
+  },
+  :host => \"localhost:3000\",
+  :basepath => \"/\",
+  :schemes => [\"http\"],
+}" > lib/vars.rb
